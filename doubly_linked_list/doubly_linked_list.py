259a68af013bd79a822fbb46c33140eb7f493cbb
Ashley Dunham <xenocide21@gmail.com>
2020-08-22 01:04:35 -0700

    Completed doubly_linked_list.py, added code to stack.py for DoublyLinkedList() and tested. All tests pass.

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -2,16 +2,21 @@
 Each ListNode holds a reference to its previous node
 as well as its next node in the List.
 """
+
+
 class ListNode:
     def __init__(self, value, prev=None, next=None):
         self.prev = prev
         self.value = value
         self.next = next
-            
+
+
 """
 Our doubly-linked list class. It holds references to 
 the list's head and tail nodes.
 """
+
+
 class DoublyLinkedList:
     def __init__(self, node=None):
         self.head = node
@@ -20,63 +25,158 @@ class DoublyLinkedList:
 
     def __len__(self):
         return self.length
-    
+
     """
     Wraps the given value in a ListNode and inserts it 
     as the new head of the list. Don't forget to handle 
     the old head node's previous pointer accordingly.
     """
+
     def add_to_head(self, value):
-        pass
-        
+        new_node = ListNode(value, None, None)
+        if not self.head and not self.tail:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head.prev = new_node
+            self.head = new_node
+        self.length += 1
+
     """
     Removes the List's current head node, making the
     current head's next node the new head of the List.
     Returns the value of the removed Node.
     """
+
     def remove_from_head(self):
-        pass
-            
+        if not self.head and not self.tail:
+            return None
+        else:
+            value = self.head.value
+            self.length -= 1
+            if self.tail is self.head:
+                self.head = None
+                self.tail = None
+            else:
+                self.head = self.head.next
+                self.head.prev = None
+            return value
+
     """
     Wraps the given value in a ListNode and inserts it 
     as the new tail of the list. Don't forget to handle 
     the old tail node's next pointer accordingly.
     """
+
     def add_to_tail(self, value):
-        pass
-            
+        new_node = ListNode(value, None, None)
+        if not self.head and not self.tail:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            new_node.prev = self.tail
+            self.tail = new_node
+        self.length += 1
+
     """
     Removes the List's current tail node, making the 
     current tail's previous node the new tail of the List.
     Returns the value of the removed Node.
     """
+
     def remove_from_tail(self):
-        pass
-            
+        if not self.head and not self.tail:
+            return None
+        else:
+            value = self.tail.value
+            self.length -= 1
+            if self.head is self.tail:
+                self.head = None
+                self.tail = None
+            else:
+                self.tail = self.tail.prev
+                self.tail.next = None
+            return value
+
     """
     Removes the input node from its current spot in the 
     List and inserts it as the new head node of the List.
     """
+
     def move_to_front(self, node):
-        pass
-        
+        if not node:
+            return
+        if self.head is self.tail:
+            return
+        if node is self.tail:
+            self.tail = self.tail.prev
+            self.tail.next = None
+        else:
+            node.prev.next = node.next
+            node.next.prev = node.prev
+        node.next = self.head
+        self.head = node
+        node.prev = None
+
     """
     Removes the input node from its current spot in the 
     List and inserts it as the new tail node of the List.
     """
+
     def move_to_end(self, node):
-        pass
+        if not node:
+            return
+        if self.head is self.tail:
+            return
+        if node is self.head:
+            self.head = self.head.next
+            self.head.prev = None
+        else:
+            node.prev.next = node.next
+            node.next.prev = node.prev
+        node.prev = self.tail
+        node.prev.next = node
+        self.tail = node
+        node.next = None
 
     """
     Deletes the input node from the List, preserving the 
     order of the other elements of the List.
     """
+
     def delete(self, node):
-        pass
+        if not node:
+            return None
+        if node is self.head and node is self.tail:
+            self.head = None
+            self.tail = None
+        else:
+            if node.prev:
+                node.prev.next = node.next
+            if node.next:
+                node.next.prev = node.prev
+            if node is self.head:
+                self.head = node.next
+            if node is self.tail:
+                self.tail = node.prev
+        self.length -= 1
 
     """
     Finds and returns the maximum value of all the nodes 
     in the List.
     """
+
     def get_max(self):
-        pass
+        head = self.head
+        if not head:
+            return 0
+        max_val = head.value
+        while head.next:
+            if head.value > max_val:
+                max_val = head.value
+            head = head.next
+        if head.value > max_val:
+            max_val = head.value
+        return max_val
--- stack/stack.py
+++ stack/stack.py
@@ -10,13 +10,36 @@ return elements in Last In First Out order.
 3. What is the difference between using an array vs. a linked list when 
    implementing a Stack?
 """
-from singly_linked_list.singly_linked_list import LinkedList
+
+"""Singly Linked List stack"""
+# from singly_linked_list.singly_linked_list import LinkedList
+
+
+# class Stack:
+#     def __init__(self):
+#         self.size = 0
+#         self.storage = LinkedList()
+#
+#     def __len__(self):
+#         return self.size
+#
+#     def push(self, value):
+#         self.size += 1
+#         self.storage.add_to_tail(value)
+#
+#     def pop(self):
+#         if self.size > 0:
+#             self.size -= 1
+#             return self.storage.remove_tail()
+
+"""Doubly Linked List stack"""
+from doubly_linked_list.doubly_linked_list import DoublyLinkedList
 
 
 class Stack:
     def __init__(self):
         self.size = 0
-        self.storage = LinkedList()
+        self.storage = DoublyLinkedList()
 
     def __len__(self):
         return self.size
@@ -28,4 +51,4 @@ class Stack:
     def pop(self):
         if self.size > 0:
             self.size -= 1
-            return self.storage.remove_tail()
+            return self.storage.remove_from_tail()
